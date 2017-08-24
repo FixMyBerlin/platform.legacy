2926cd3ab92bb1564601e7294437391d4d0582bb
Boris Hekele <boris@fixmyberlin.de>
2017-08-24 23:42:10 +0200

    Add google spreadsheet connector

---
+++ importer/index.php
@@ -0,0 +1,75 @@
+<?php
+
+#require_once 'init.php';
+#$spreadsheet = $service->spreadsheets_values->get($fileId, 'Radverkehrsprojekte');
+#$spreadsheet->values;
+#file_put_contents('test.json', json_encode($spreadsheet->values, JSON_PRETTY_PRINT));
+
+define('DRUPAL_DIR', dirname(__DIR__));
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+require_once DRUPAL_DIR . '/core/includes/database.inc';
+require_once DRUPAL_DIR . '/core/includes/schema.inc';
+
+// specify relative path to the drupal root.
+$autoloader = require_once DRUPAL_DIR . '/autoload.php';
+$request = Request::createFromGlobals();
+
+// bootstrap drupal to different levels
+$kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
+$kernel->boot();
+$kernel->prepareLegacyRequest($request);
+
+use \Drupal\node\Entity\Node;
+use \Drupal\node\Entity\TaxonomyTerm;
+
+$values = json_decode(file_get_contents('test.json'));
+
+// identify column names
+foreach($values as &$row) {
+  foreach($row as &$cell) {
+    $cell = trim($cell);
+    if($cell == "UUID") {
+      $index = array_flip($row);
+    }
+  }
+}
+
+//
+foreach($values as $row) {
+
+  // check if do import is true and UUID is in valid format
+  if(@$row[$index['do_import']] == 1 && preg_match('/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/', @$row[$index['UUID']])) {
+    $uuid = $row[$index['UUID']];
+    $title = $row[$index['Adresse']];
+    $body = $row[$index['Beschreibung der Maßnahme']];
+    $project_number = $row[$index['Projektnummer']];
+
+    // status
+    $status = $row[$index['Status']];
+    $terms = taxonomy_term_load_multiple_by_name($status, 'project_status');
+    if(!empty($terms)) {
+      $term_status = array_pop($terms);
+    }
+    else {
+      $term_status = \Drupal\taxonomy\Entity\Term::create([
+        'vid' => 'project_status',
+        'name' => $status,
+      ]);
+      $term_status->save();
+    }
+
+    // create entity if empty
+    if(!($entity = \Drupal::entityManager()->loadEntityByUuid('node', $uuid))) {
+      $entity = Node::create([
+        'type' => 'project',
+        'uuid' => $uuid,
+      ]);
+    }
+    $entity->title = $title;
+    $entity->body = $body;
+    $entity->field_project_status = $term_status->id();
+    $entity->field_project_number = $project_number;
+    $entity->save();
+  }
+}
---
+++ importer/init.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'google-api-php-client/vendor/autoload.php';
+$client = new Google_Client();
+$client->setAuthConfig('../../FixMyBerlin-9c2989a86d05.json');
+$client->setApplicationName("FixMyBerlin Sheets API");
+$client->useApplicationDefaultCredentials();
+$client->addScope('https://spreadsheets.google.com/feeds');
+$service = new Google_Service_Sheets($client);
